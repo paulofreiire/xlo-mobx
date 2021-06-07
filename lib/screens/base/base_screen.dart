4f9adb18455071bcdbd5fb95b93e76614c64351f
Paulo Freire <paulo.freiire@hotmail.com>
2021-06-07 14:57:44 -0300

    add mobx

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
 import 'package:xlo/screens/home/home_screen.dart';
+import 'package:xlo/stores/page_store.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
 
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
+
+  final PageStore pageStore = PageStore();
+
+  @override
+  void initState() {
+    super.initState();
+
+    reaction(
+        (_) => pageStore.page,
+        (page) => pageController.jumpToPage(page)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
